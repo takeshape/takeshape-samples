6c9c2f9def54ad25daba11ae908ee5171328df6d
Allan Lasser <lasser.allan@gmail.com>
2018-11-02 10:27:41 -0400

    Adds source stiching to Gatsby config

--- shape-portfolio-gatsbyjs/gatsby-config.js
+++ shape-portfolio-gatsbyjs/gatsby-config.js
@@ -25,6 +25,22 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "TS",
+        fieldName: "takeshape",
+        // Url to query from
+        url: `https://api.takeshape.io/project/${process.env.TAKESHAPE_PROJECT}/graphql`,
+        // HTTP headers
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.TAKESHAPE_TOKEN}`,
+        },
+        // Additional options to pass to node-fetch
+        fetchOptions: {},
+      },
+    },
     'gatsby-plugin-offline',
   ],
 }
