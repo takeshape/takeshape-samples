c75b8a9ded004f3c1432dcf6734178eb8ae79826
Allan Lasser <lasser.allan@gmail.com>
2018-11-07 17:06:41 -0500

    Loads projects from TakeShape and lists them on the homepage

---
+++ shape-portfolio-gatsbyjs/src/components/ProjectList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import {getImageUrl} from 'takeshape-routing';
+
+const ProjectListItem = ({project}) => {
+  const startYear = new Date(project.startDate).getFullYear();
+  const endYear = project.endDate ? new Date(project.endDate).getFullYear() : null;
+  const includeEndYear = endYear && startYear !== endYear
+  return (
+    <Link to="#project" className="project">
+      <img className="project__thumbnail" src={getImageUrl(project.coverImage.path, {h: 200, w: 300, fit: 'crop'})} />
+      <p className="project__name"><strong>{project.name}</strong></p>
+      <p class="project__metadata">{startYear}{includeEndYear ? `&endash; ${endYear}` : ''}</p>
+    </Link>
+  )
+};
+
+const ProjectList = ({projects}) => (
+  <ul className="nostyle project-list">
+    {projects.items.map((project, i) => (
+      <li className="project-list--entry" key={i}>
+        <ProjectListItem project={project} />
+      </li>
+    ))}
+  </ul>
+)
+
+export default ProjectList;
+
+/*
+<a class="project project-list--entry" href="{{ project|route('project') }}" title="{{ project.name }}">
+  <img class="project__thumbnail" src="{{ project.coverImage.path|image({h: 200, w: 300, fit: 'crop'}) }}">
+  <p class="project__name"><strong>{{ project.name }}</strong></p>
+  <p
+    class="project__metadata">{{ project.startDate|date('YYYY') }}{% if not project.endDate and project.startDate|date('YYYY') !== project.endDate|date('YYYY') %} – {% if project.endDate %}{{ project.endDate|date('YYYY') }}{% else %}Present{% endif %}{% endif %}</p>
+</a>
+*/
--- shape-portfolio-gatsbyjs/src/pages/index.js
+++ shape-portfolio-gatsbyjs/src/pages/index.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 
 import Layout from '../layouts/default'
-import Image from '../components/image'
+import ProjectList from '../components/ProjectList'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <ProjectList projects={data.takeshape.projects} />
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    takeshape {
+      projects: getProjectList {
+        items {
+          name
+          startDate
+          coverImage {
+            path
+          }
+        }
+      }
+    }
+  }
+`
