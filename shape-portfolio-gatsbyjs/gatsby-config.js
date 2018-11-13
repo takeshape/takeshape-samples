424de968f80add05baff97388acffb4ce16ecf21
mcat <catalano.m@gmail.com>
2018-11-12 20:18:34 -0500

    use takeshape icon as favicon

--- shape-portfolio-gatsbyjs/gatsby-config.js
+++ shape-portfolio-gatsbyjs/gatsby-config.js
@@ -1,48 +1,47 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: "Gatsby Default Starter"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#5439D2",
+        theme_color: "#5EDEB3",
+        display: "minimal-ui",
+        icon: "src/images/takeshape-icon.png"
+      }
     },
     {
       resolve: "gatsby-source-graphql",
       options: {
         typeName: "TS",
         fieldName: "takeshape",
-        // Url to query from
-        url: `https://api.takeshape.io/project/${process.env.TAKESHAPE_PROJECT}/graphql`,
-        // HTTP headers
+        url: `https://api.takeshape.io/project/${
+          process.env.TAKESHAPE_PROJECT
+        }/graphql`,
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.TAKESHAPE_TOKEN}`,
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.TAKESHAPE_TOKEN}`
         },
-        // Additional options to pass to node-fetch
-        fetchOptions: {},
-      },
+        fetchOptions: {}
+      }
     },
-    'gatsby-plugin-offline',
-  ],
+    "gatsby-plugin-offline"
+  ]
 };
