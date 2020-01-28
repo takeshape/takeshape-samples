9876170f13564cb282ebf2ef2dc8daa1b59d63da
Allan Lasser <lasser.allan@gmail.com>
2020-01-28 11:00:31 -0500

    Add next.js sample project

---
+++ shape-blog-nextjs/src/pages/api/posts/index.js
@@ -0,0 +1,7 @@
+import TakeShape from '../../../providers/takeshape'
+import {postListQuery} from '../../posts'
+import {getTakeShapeData} from '../index'
+
+export default async (req, res) => {
+	return getTakeShapeData(req, res, postListQuery)
+}
